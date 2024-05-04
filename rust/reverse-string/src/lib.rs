75d54eabcfc590fbacf467e63c5a97c30c8a18ad
E McGill <elina.mcgill@theinclab.com>
2024-05-03 22:07:34 -0400

    ✏️ lint: lint code using rustfmt

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -3,6 +3,6 @@ use unicode_segmentation::UnicodeSegmentation;
 pub fn reverse(input: &str) -> String {
     let mut graphemes: Vec<&str> = UnicodeSegmentation::graphemes(input, true).collect();
     graphemes.reverse();
-    let reversed = graphemes.concat(); 
+    let reversed = graphemes.concat();
     reversed
 }
